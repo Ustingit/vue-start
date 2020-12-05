85f6ab929cf5b4054108b2de0053b04a85397d68
ustinovich.yuryi <ustinhtc@gmail.com>
2020-12-05 16:42:11 +0300

    finish fetching and new life-cycle method mounted

--- src/App.vue
+++ src/App.vue
@@ -24,15 +24,11 @@ export default {
   },
   data() {
     return {
-      todos: [
-        { id: 1, title: "Купить хлеб", completed: false },
-        { id: 2, title: "Купить масло", completed: false },
-        { id: 3, title: "Купить сыр", completed: false }
-      ]
+      todos: []
     }
   },
   mounted() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(response => response.json())
       .then(json => this.todos = json);
   },
